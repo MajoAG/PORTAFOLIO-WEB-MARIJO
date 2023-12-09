375ed905b635cd83d4d9fe771f286227559d6b74
MajoAG <153387614+MajoAG@users.noreply.github.com>
2023-12-09 13:37:09 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let app=document.getElementById('typewriter')
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+    
+
+} ) ;
+typewriter 
+.pauseFor (2500)
+.typeString ('<span>Hola, ella es Taylor Swift </span>')
+.pauseFor (200)
+.deleteChars (10)
+.start ();
+
+
+
+
